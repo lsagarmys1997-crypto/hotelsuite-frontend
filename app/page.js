4dc59155f43042ca32837d1169cceb65d18a8105
lsagarmys1997-crypto <l.sagar.mys1997@gmail.com>
2025-12-19 15:49:16 +0530

    page.jsx

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>HotelSuite Frontend</h1>
-      <p>Frontend is live 🚀</p>
-      <p>Backend API connected on Render</p>
-    </main>
-  );
-}
